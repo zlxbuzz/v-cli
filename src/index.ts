80ce293b99b476a9140e98d3654faf64f734003c
zlxbuzz <824665478@qq.com>
2024-03-07 17:32:09 +0800

    feat: add ts config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export const hello = () => { };
+#!/usr/bin/env node
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .action((message: string) => {
+    console.log(`Hello World ${message}`);
+  })
+  .argument("<string>", "console log");
+
+program.parse(process.argv);
